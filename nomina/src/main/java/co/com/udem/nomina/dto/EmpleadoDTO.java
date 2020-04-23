f1be4aaf85b14903428e477dd019879c9da368dd
Your Name <you@example.com>
2020-04-22 22:32:11 -0500

    Setup inicial del proyecto y DTO de empleado

---
+++ nomina/src/main/java/co/com/udem/nomina/dto/EmpleadoDTO.java
@@ -0,0 +1,50 @@
+package co.com.udem.nomina.dto;
+
+public class EmpleadoDTO {
+	private String nombre;
+	private String apellido;
+	private String cedula;
+	private String departamento;
+	private Double salario;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+}
